7983aecc4b10c5c28bc22ce61ab35954a6b387e3
Nadya <nad.anikina@gmail.com>
2022-10-05 20:35:22 +0300

    Задача2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿//max из 2х чисел
+/*
 int number1 = int.Parse(Console.ReadLine());
 int number2 = int.Parse(Console.ReadLine());
 if (number1>number2) Console.WriteLine($"max = {number1},min= {number2}");
 else Console.WriteLine($"max = {number2},min= {number1}");
+*/
 
-    
+//max из 3х чисел
 
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+if (number1>number2&&number1>number3) Console.WriteLine("max ="+number1);
+else if (number2>number1&&number2>number3) Console.WriteLine("max ="+number2); 
+else if (number3>number1&&number3>number2) Console.WriteLine("max ="+number3); 

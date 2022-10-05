d4f511c1c46543bc720b8e36406c2979250fc6c7
Nadya <nad.anikina@gmail.com>
2022-10-05 20:21:32 +0300

    Задача 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//max из 2х чисел
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+if (number1>number2) Console.WriteLine($"max = {number1},min= {number2}");
+else Console.WriteLine($"max = {number2},min= {number1}");
 
-string name;
-name = Console.ReadLine();
-Console.WriteLine("enter the number");
-int number = int.Parse(Console.ReadLine());
-Console.WriteLine($"square {number} is equal {number*number}");
-Console.WriteLine("Your name "+name);
+    
 

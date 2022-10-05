be68463097a218886111a7b78f36a54cfe79d522
Nadya <nad.anikina@gmail.com>
2022-10-05 20:44:57 +0300

    Задача3

--- Program.cs
+++ Program.cs
@@ -7,10 +7,16 @@ else Console.WriteLine($"max = {number2},min= {number1}");
 */
 
 //max из 3х чисел
-
+/*
 int number1 = int.Parse(Console.ReadLine());
 int number2 = int.Parse(Console.ReadLine());
 int number3 = int.Parse(Console.ReadLine());
 if (number1>number2&&number1>number3) Console.WriteLine("max ="+number1);
 else if (number2>number1&&number2>number3) Console.WriteLine("max ="+number2); 
 else if (number3>number1&&number3>number2) Console.WriteLine("max ="+number3); 
+*/
+
+//является ли число четным
+int number1 = int.Parse(Console.ReadLine());
+if (number1 % 2==0) Console.WriteLine("Число четное");
+else Console.WriteLine("Число нечетное");

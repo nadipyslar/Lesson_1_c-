ddfb29a594cb1f5e4bc646273c5f91b304324e1e
Nadya <nad.anikina@gmail.com>
2022-10-23 23:42:22 +0300

    2 task

--- Program.cs
+++ Program.cs
@@ -225,7 +225,7 @@ DiffMaxMin(randomArray);
 */
 
 //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-Console.Write("Введите элементы через пробел: ");
+/*Console.Write("Введите элементы через пробел: ");
 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = 0;
 for (int i = 0; i < arr.Length; i++)
@@ -234,4 +234,20 @@ for (int i = 0; i < arr.Length; i++)
         count++;
 }
 Console.WriteLine($"Кол-во элементов > 0: {count}");
+*/
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double numberX = (b2 - b1)/(k1 - k2);
+double numberY = k2 * numberX + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых: {numberX},{numberY}");
 

b2f50b6167002db60868e491993679906dc5324e
Nadya <nad.anikina@gmail.com>
2022-11-09 23:58:04 +0300

    2task7lesson

--- Program.cs
+++ Program.cs
@@ -257,7 +257,7 @@ Console.WriteLine($"Точка пересечения двух прямых: {nu
 //0,5 7 -2 -0,2
 //1 -3,3 8 -9,9
 //8 7,8 -7,1 9
-Console.WriteLine("введите количество строк массива");
+/*Console.WriteLine("введите количество строк массива");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите количество столбцов массива");
 int columns = Convert.ToInt32(Console.ReadLine());
@@ -277,5 +277,36 @@ void PrintArray(double [,]array){
         Console.Write(array[i, j] + " ");
         Console.Write("]");
         Console.WriteLine("");
+    }
+}
+*/
+//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента
+//или же указание, что такого элемента нет.
+
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+    Console.WriteLine("такого элемента нет");
+else
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+PrintArray(numbers);
+void FillArrayRandomNumbers(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+                array [i,j] = new Random().Next(-100, 100)/10;
+               
+        }
+void PrintArray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++){
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+           
+        Console.Write("]");
+        Console.WriteLine(""); 
     }
 }

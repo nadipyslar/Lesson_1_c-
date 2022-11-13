5d920fe63b9ae56910cae44135cb0e49f89faa6b
Nadya <nad.anikina@gmail.com>
2022-11-13 18:53:42 +0300

    4 task

--- Program.cs
+++ Program.cs
@@ -513,13 +513,10 @@ int[,,] array3D = new int[x,y,z];
 FillArray(array3D);
 PrintArray(array3D);
 void PrintArray (int[,,] array3D){
-  for (int i = 0; i < array3D.GetLength(0); i++)
-  {
-    for (int j = 0; j < array3D.GetLength(1); j++)
-    {
+  for (int i = 0; i < array3D.GetLength(0); i++){
+    for (int j = 0; j < array3D.GetLength(1); j++){
       Console.Write($"X({i}) Y({j}) ");
-      for (int k = 0; k < array3D.GetLength(2); k++)
-      {
+        for (int k = 0; k < array3D.GetLength(2); k++){
         Console.Write( $"Z({k})={array3D[i,j,k]}; ");
       }
       Console.WriteLine();
@@ -534,22 +531,19 @@ void FillArray(int[,,] array3D){
     temp[i] = new Random().Next(10, 100);
     number = temp[i];
     if (i >= 1)
-      for (int j = 0; j < i; j++)
-      {
-        while (temp[i] == temp[j])
-        {
-          temp[i] = new Random().Next(10, 100);
-          j = 0;
-          number = temp[i];
+      for (int j = 0; j < i; j++){
+        while (temp[i] == temp[j]){
+        temp[i] = new Random().Next(10, 100);
+        j = 0;
+        number = temp[i];
         }
-          number = temp[i];
+        number = temp[i];
       }
   }
   int count = 0; 
   for (int x = 0; x < array3D.GetLength(0); x++){
     for (int y = 0; y < array3D.GetLength(1); y++)
-    for (int z = 0; z < array3D.GetLength(2); z++)
-      {
+    for (int z = 0; z < array3D.GetLength(2); z++){
         array3D[x, y, z] = temp[count];
         count++;
       }

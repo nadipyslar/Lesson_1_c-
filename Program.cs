57580896c0f6bb01ec16a67e50cccd2fe4eca983
Nadya <nad.anikina@gmail.com>
2022-11-13 17:52:46 +0300

    1 task

--- Program.cs
+++ Program.cs
@@ -312,6 +312,7 @@ void PrintArray(int[,] array){
 }
 */
 //Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
+/*
 Console.WriteLine("введите количество строк массива");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите количество столбцов массива");
@@ -344,3 +345,50 @@ void PrintArray(int [,]array){
         Console.WriteLine("");
     }
 }
+*/
+
+//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива
+Console.WriteLine("введите количество строк массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+PrintArray(array);
+
+void FillArray(int[,] array){
+    for (int i = 0; i<array.GetLength(0); i++)
+        for (int j = 0; j<array.GetLength(1); j++)
+            array[i,j] = Convert.ToInt32(new Random().Next(-100, 100)) / 10;
+}
+
+void PrintArray(int [,]array){
+    for (int i = 0; i < array.GetLength(0); i++){
+    Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + " ");
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+
+

ca0964475493c9959db116a0aca12cc97483437d
Nadya <nad.anikina@gmail.com>
2022-11-13 18:49:47 +0300

    4 task

--- Program.cs
+++ Program.cs
@@ -446,7 +446,7 @@ void PrintArray(int [,]array){
 */
 
 // Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-
+/*
 Console.Write("Введите количество строк 1 массива: ");
 int rowsA = int.Parse(Console.ReadLine());
 Console.Write("Введите количество столбцов 1 массива: ");
@@ -496,6 +496,63 @@ void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatr
       }
       resultMatrix[i,j] = sum;
     }
+  }
+}
+*/
+
+//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента
+
+Console.WriteLine("введите X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Y");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Z");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] array3D = new int[x,y,z];
+FillArray(array3D);
+PrintArray(array3D);
+void PrintArray (int[,,] array3D){
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      Console.Write($"X({i}) Y({j}) ");
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write( $"Z({k})={array3D[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+void FillArray(int[,,] array3D){
+  int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++){
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+  }
+  int count = 0; 
+  for (int x = 0; x < array3D.GetLength(0); x++){
+    for (int y = 0; y < array3D.GetLength(1); y++)
+    for (int z = 0; z < array3D.GetLength(2); z++)
+      {
+        array3D[x, y, z] = temp[count];
+        count++;
+      }
   }
 }
 
